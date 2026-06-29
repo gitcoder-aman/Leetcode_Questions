84812d3d48a4fd89ae32d621be209a769972d7cc
Aman Kumar <96575890+gitcoder-aman@users.noreply.github.com>
2026-06-29 11:11:14 +0530

    Time: 1 ms (23.01%), Space: 42.9 MB (31.73%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean checkValidString(String s) {
-        int cmin = 0, cmax = 0;
+     /*   int cmin = 0, cmax = 0;
         for(char c : s.toCharArray()){
             if(c == '('){
                 cmax++;
@@ -16,6 +16,49 @@ class Solution {
 
             cmin = Math.max(cmin,0);
         }
-        return cmin == 0;
+        return cmin == 0;*/
+
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                open.push(i);
+            } 
+            else if (ch == '*') {
+                star.push(i);
+            } 
+            else { // ch == ')'
+
+                // First try to match with '('
+                if (!open.isEmpty()) {
+                    open.pop();
+                }
+                // Otherwise use '*'
+                else if (!star.isEmpty()) {
+                    star.pop();
+                }
+                // Neither '(' nor '*' available
+                else {
+                    return false;
+                }
+            }
+        }
+
+        // Match remaining '(' with '*' that come after them
+        while (!open.isEmpty() && !star.isEmpty()) {
+
+            if (open.peek() < star.peek()) {
+                open.pop();
+                star.pop();
+            } else {
+                // '*' is before '(', so it can't act as ')'
+                return false;
+            }
+        }
+
+        return open.isEmpty();
     }
 }
