5f1b3a4cd063b4dea20f89203cead292cdc2f22d
Aman Kumar <96575890+gitcoder-aman@users.noreply.github.com>
2026-07-18 12:17:19 +0530

    Time: 0 ms (100%), Space: 45 MB (38.16%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -12,26 +12,47 @@ class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         
         if(head == null || head.next == null) return head;
-        HashMap<Integer,Integer>map = new HashMap<>();
+        // HashMap<Integer,Integer>map = new HashMap<>();
+        
+        // ListNode curr = head;
+
+        // while(curr != null){
+        //     map.put(curr.val, map.getOrDefault(curr.val, 0) + 1);
+        //     curr = curr.next;
+        // }
+        // ListNode dummy = new ListNode(0);
+        // ListNode tail = dummy;
+        // curr = head;
+
+        // while(curr != null){
+        //     if(map.get(curr.val) == 1){
+        //         tail.next = curr;
+        //         tail = tail.next;
+        //     }
+        //     curr = curr.next;
+        // }
+        // tail.next = null;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
         
         ListNode curr = head;
-
+        ListNode prev = dummy;
         while(curr != null){
-            map.put(curr.val, map.getOrDefault(curr.val, 0) + 1);
-            curr = curr.next;
-        }
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-        curr = head;
+            if(curr.next != null && curr.val == curr.next.val){
 
-        while(curr != null){
-            if(map.get(curr.val) == 1){
-                tail.next = curr;
-                tail = tail.next;
+                //skip all node with the same values
+                while(curr.next != null && curr.val == curr.next.val){
+                    curr = curr.next;
+                }
+                //skip the last duplicate as well
+                curr = curr.next;
+                prev.next = curr;
+            }else{
+                prev = curr;
+                curr = curr.next;
             }
-            curr = curr.next;
         }
-        tail.next = null;
         return dummy.next;
     }
 }
